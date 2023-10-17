5244ba5cce33ab9dd31ff38d87e783ba0fbbdf0d
Toni <toniperalb@gmail.com>
2023-10-17 09:42:55 +0200

    producto hecho

---
+++ src/app/store/js/Producto.js
@@ -0,0 +1,75 @@
+class Producto {
+  constructor(
+    titulo = "",
+    descripcion = "",
+    precio = 0.0,
+    opiniones = [],
+    puntuacion_Media = 0.0,
+    oferta = false,
+    num_Ventas = 0
+  ) {
+    this._titulo = titulo;
+    this._descripcion = descripcion;
+    this._precio = precio;
+    this._opiniones = opiniones;
+    this._puntuacion_Media = puntuacion_Media;
+    this._oferta = oferta;
+    this._num_Ventas = num_Ventas;
+  }
+
+  get titulo() {
+    return this._titulo;
+  }
+
+  set titulo(value) {
+    this._titulo = value;
+  }
+
+  get descripcion() {
+    return this._descripcion;
+  }
+
+  set descripcion(value) {
+    this._descripcion = value;
+  }
+
+  get precio() {
+    return this._precio;
+  }
+
+  set precio(value) {
+    this._precio = value;
+  }
+
+  get opiniones() {
+    return this._opiniones;
+  }
+
+  set opiniones(value) {
+    this._opiniones = value;
+  }
+
+  get puntuacion_Media() {
+    return this._puntuacion_Media;
+  }
+
+  set puntuacion_Media(value) {
+    this._puntuacion_Media = value;
+  }
+
+  get oferta() {
+    return this._oferta;
+  }
+
+  set oferta(value) {
+    this._oferta = value;
+  }
+
+  get num_Ventas() {
+    return this._num_Ventas;
+  }
+
+  set num_Ventas(value) {
+    this._num_Ventas = value;
+  }
+}
