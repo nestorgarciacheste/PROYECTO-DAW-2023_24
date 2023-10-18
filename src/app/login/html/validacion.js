8d08095dae0e5b099641d515aeb2faec36906dab
ceciliacortess <cecorvel2004@gmail.com>
2023-10-18 11:55:05 +0200

    Cambios validación

---
+++ src/app/login/html/validacion.js
@@ -0,0 +1,15 @@
+function validacion(){
+    'use strict'
+    forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+}
