62a353b5fb3a21f4f14e634d22449f96364d467f
Toni <toniperalb@gmail.com>
2023-10-25 11:58:56 +0200

    11/58

--- src/app/store/js/Main.js
+++ src/app/store/js/Main.js
@@ -1,5 +1,5 @@
-import producto from "./Product";
 import { Opinion } from "./Opinion";
+import Product from "./Product";
 
 var productList = [];
 var selectedProductID = 0;
@@ -33,4 +33,51 @@ function deleteProduct(product) {
 
 function selectProduct(id) {
   selectedProductID = id;
+}
+
+function createProducts() {
+  var data = [
+    "Introduction to Programming",
+    "This course provides the fundamentals of programming, including basic concepts of algorithms and data structures.",
+    "Front-end Web Development",
+    "Focuses on creating the user interface of websites using technologies like HTML, CSS, and JavaScript.",
+    "Back-end Web Development",
+    "Centered on creating server logic and managing databases for web applications.",
+    "User Interface Design",
+    "Teaches how to create attractive and functional interfaces for applications and websites.",
+    "Databases and SQL",
+    "Covers database management and the SQL language for storing and retrieving data.",
+    "Python Programming",
+    "Introduces the Python programming language and its applications in software development, data analysis, and more.",
+  ];
+
+  var idProduct = 0;
+  for (let i = 0; i < 12; i += 2) {
+    let o = new Product(
+      idProduct,
+      data[i],
+      data[i + 1],
+      getRandomNumber(30, 300),
+      isOnSale()
+    );
+    idProduct++;
+    add_Product(o);
+  }
+}
+
+function getRandomNumber(min, max) {
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNum;
+}
+
+function isOnSale() {
+  const randomNum = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+  if (randomNum === 1) {
+    return false;
+  }
+  return true;
 }
