e1d3c8f806b3b7857fe08c049311780b246fd342
ceciliacortess <cecorvel2004@gmail.com>
2023-10-25 14:31:24 +0200

    cambios formulario y validacion

--- src/app/login/html/validacion.js
+++ src/app/login/html/validacion.js
@@ -1,57 +1,55 @@
-(function() {
-    'use strict';
+function validacion() {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.from(forms).forEach((form) => {
+    form.addEventListener("submit", function (event) {
+      // Validación email
+      var email = form.querySelector('[name="email"]').value;
+      var emailPat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    var forms = document.querySelectorAll('.needs-validation');
+      if (!emailPat.test(email)) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
 
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else{
-                // Validación email
-                var email = form.querySelector('[name="email"]').value;
+      // Validación contraseña
+      var password = form.querySelector('[name="password"]').value;
+      var confPassword = form.querySelector('[name="confPassword"]').value;
+      var contPat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-                if (!isValidEmail(email)) {
-                    event.preventDefault();
-                }
+      if (!contPat.test(password)) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
 
-                //Validación contraseña
-                var password = form.querySelector('[name="password"]').value;
-                var confPassword = form.querySelector('[name="confPassword"]').value;
+      if (password !== confPassword) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
 
-                if (!isValidPassword(password)) {
-                    event.preventDefault();
-                }
+      // Validación teléfono
+      var telefono = form.querySelector('[name="telefono"]').value;
+      var telPat = /^\d{9}$/;
 
-                if (password !== confPassword) {
-                    event.preventDefault();
-                }
+      if (!telPat.test(telefono)) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
 
-                //Validación teléfono
-                var telef = form.querySelector('[name="telefono"]').value;
+      // Validación código postal
+      var zip = form.querySelector('[name="zip"]').value;
+      var zipPat = /^\d{5}$/;
 
-                if (!isValidPhoneNumber(telef)) {
-                    event.preventDefault();
-                }
-            }
-
-            form.classList.add('was-validated');
-        }, false);
+      if (!zipPat.test(zip)) {
+        event.preventDefault();
+        event.stopPropagation();
+        return;
+      }
     });
-
-    function isValidEmail(email) {
-        emailPat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        return emailPattern.test(email);
-    }
-
-    function isValidPassword(password) {
-        contPat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-        return contPat.test(password);
-    }
-
-    function isValidPhoneNumber(telefono) {
-        telPat = /^\d{9}$/;
-        return regex.test(telefono);
-    }
-})();
+    form.classList.add("was-validated");
+  });
+}
