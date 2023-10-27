40941366742408f57e8a207f85f8b039e8c4f7b6
DeniseGarrido <garrido.tamarit.denise@gmail.com>
2023-10-27 17:09:19 +0200

    auth0 github

--- src/app/login/html/app.js
+++ src/app/login/html/app.js
@@ -13,13 +13,15 @@ function loginWithGoogle() {
 }
 
 function loginWithGitHub() {
-  const redirectUrl = 'http://localhost:5500/src/app/home/html/index.html';
-  const responseType = 'code';
-  const connection = 'github';
+    const redirectUrl = 'http://localhost:5500/src/app/home/html/index.html';
+    const responseType = 'code';
+    const connection = 'github';
 
-  const auth0Domain = 'dev-nt6cgad86rkuabgq.us.auth0.com';
-  const clientId = '15dHTMDSUZSYnvvxipcxTq70ccemkgC2';
-  const authorizeUrl = `https://${auth0Domain}/authorize?response_type=${responseType}&client_id=${clientId}&connection=${connection}&redirect_uri=${redirectUrl}`;
+    const auth0Domain = 'dev-nt6cgad86rkuabgq.us.auth0.com';
+    const clientId = '15dHTMDSUZSYnvvxipcxTq70ccemkgC2';
+    const authorizeUrl = `https://${auth0Domain}/authorize?response_type=${responseType}&client_id=${clientId}&connection=${connection}&redirect_uri=${redirectUrl}`;
 
-  const popupWindow = window.open(authorizeUrl, 'GitHubAuthentication', 'width=600,height=600');
+    window.location.href = authorizeUrl;
 }
+
+  
