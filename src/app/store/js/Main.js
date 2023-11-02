ca2b4d988623f0cae8b0e301d878d64737d71ccf
Toni <toniperalb@gmail.com>
2023-11-02 10:39:02 +0100

    html

--- src/app/store/js/Main.js
+++ src/app/store/js/Main.js
@@ -1,5 +1,26 @@
 const Product = require("./Product");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const headerContainer = document.getElementById("header-container");
+  const footerContainer = document.getElementById("footer-container");
+  fetch("../../header_footer/html/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      headerContainer.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el header:", error);
+    });
+  fetch("../../header_footer/html/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      footerContainer.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el footer:", error);
+    });
+});
+
 const productList = [];
 let selectedProductID = 0;
 
