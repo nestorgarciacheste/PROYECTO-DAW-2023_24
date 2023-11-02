a5463f03689b53222f29d1df0f87478c179c78a9
Toni <toniperalb@gmail.com>
2023-11-02 10:34:00 +0100

    Cambio

--- src/app/store/js/Main.js
+++ src/app/store/js/Main.js
@@ -1,5 +1,25 @@
-import { Opinion } from "./Opinion";
 import Product from "./Product";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const headerContainer = document.getElementById("header-container");
+  const footerContainer = document.getElementById("footer-container");
+  fetch("../../")
+    .then((response) => response.text())
+    .then((data) => {
+      headerContainer.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el header:", error);
+    });
+  fetch("../../header_footer/html/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      footerContainer.innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el footer:", error);
+    });
+});
 
 const productList = [];
 let selectedProductID = 0;
