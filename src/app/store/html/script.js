f7eb981f53ab464e33d6f2f741507b1b861cb309
Carloscontacto4d@gmail.com <carloscontacto4d@gmail.com>
2023-11-15 09:45:02 +0100

    pagina 15/11

--- src/app/store/html/script.js
+++ src/app/store/html/script.js
@@ -19,7 +19,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-/*  Barra lateral termina aqui */
+/* Barra lateral comienza aquí */
+
 document.addEventListener("DOMContentLoaded", function (event) {
   const showNavbar = (toggleId, navId, bodyId, headerId) => {
     const toggle = document.getElementById(toggleId),
@@ -42,18 +43,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
   };
 
-  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
-
-  /*===== LINK ACTIVE =====*/
-  const linkColor = document.querySelectorAll(".nav_link");
-
-  function colorLink() {
-    if (linkColor) {
-      linkColor.forEach((l) => l.classList.remove("active"));
-      this.classList.add("active");
-    }
-  }
-  linkColor.forEach((l) => l.addEventListener("click", colorLink));
-
-  // Your code to run since DOM is loaded and ready
+  // Call showNavbar with appropriate parameters
+  showNavbar("toggleId", "navId", "bodyId", "headerId");
 });
+
+function hideAllCards() {
+  const cards = document.querySelectorAll(".card");
+  for (const card of cards) {
+    card.style.display = "none";
+  }
+}
+
+// Agregar evento de clic a cada botón
+const buttons = document.querySelectorAll(".btn-custom");
+for (const button of buttons) {
+  button.addEventListener("click", function() {
+    // Obtener la categoría del botón
+    const category = button.dataset.category;
+
+    // Ocultar todas las cartas que no pertenecen a la categoría
+    hideAllCards();
+
+    // Mostrar las cartas que pertenecen a la categoría
+    const cards = document.querySelectorAll(".card[data-category='" + category + "']");
+    for (const card of cards) {
+      card.style.display = "block";
+    }
+  });
+}
