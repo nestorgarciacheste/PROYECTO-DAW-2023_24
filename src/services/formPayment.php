63b4b4e310d806546dfde5b54e77f1c48f50137b
DeniseGarrido <garrido.tamarit.denise@gmail.com>
2023-11-17 19:11:02 +0100

    fixing email

--- src/services/formPayment.php
+++ src/services/formPayment.php
@@ -1,38 +1,36 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Esto permite solicitudes desde cualquier origen. En producción, deberías especificar el dominio permitido.
+header("Access-Control-Allow-Origin: *"); 
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recuperar la dirección de correo electrónico del cuerpo de la solicitud
     $email = isset($_POST["email"]) ? $_POST["email"] : "";
 
     if (!empty($email)) {
-        // Configurar el correo electrónico
         $destinatario = $email;
-        $asunto = "Nuevo pedido Web Code";
+        $asunto = "New order from Web Code";
 
-        // Construir el cuerpo del correo
-        $cuerpoCorreo = "Hola, somos de WebCode\n";
-        $cuerpoCorreo .= "Te damos la gracias por tu compra\n";
-        $cuerpoCorreo .= "En menos de 24h tendras tu pedido.\n";
+        $cuerpoCorreo = "Hello, this is the WebCode team\n";
+        $cuerpoCorreo .= "We sincerely thank you for your purchase with us.\n";
+        $cuerpoCorreo .= "Your order is being processed and will be delivered within the next 24 hours.\n";
+        $cuerpoCorreo .= "We appreciate your trust in our services and hope you enjoy your products.\n";
+        $cuerpoCorreo .= "If you have any further questions or need assistance, feel free to contact us.\n";
+        $cuerpoCorreo .= "Thank ypu once again for choosing WebCode.";
+        $cuerpoCorreo .= "WebCode";
 
-        // Configurar el remitente fijo
         $remitente = "contact@webcode.intecmedia.com";
 
-        // Enviar el correo electrónico
         $headers = "From: $remitente";
 
         if (mail($destinatario, $asunto, $cuerpoCorreo, $headers)) {
-            // Enviar una respuesta al cliente
-            echo "Correo enviado con éxito";
+            echo "Email sent successfully";
         } else {
-            echo "Error al enviar el correo";
+            echo "Error sending email";
         }
     } else {
-        echo "La dirección de correo electrónico no se proporcionó correctamente.";
+        echo "The email address was not provided correclty.";
     }
 } else {
-    http_response_code(405); // Método no permitido
-    echo "Método no permitido";
+    http_response_code(405); 
+    echo "Method not allowed";
 }
 ?>
