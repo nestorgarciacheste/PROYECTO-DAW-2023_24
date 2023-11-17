317e199e721ccf03b708e15952b0216765fe3968
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-11-17 09:56:05 +0100

    17/11/2023

--- src/app/store/js/Comprar.js
+++ src/app/store/js/Comprar.js
@@ -4,7 +4,7 @@ function checkform() {
     if (emailRegex.test(email)) {
       return true;
     } else {
-      Swal.fire('Error', 'Please enter a valid e-mail.', 'error');
+      Swal.fire("Error", "Please enter a valid e-mail.", "error");
       return false;
     }
   }
@@ -13,7 +13,7 @@ function checkform() {
     if (phoneNumber.test(numero)) {
       return true;
     } else {
-      Swal.fire('Error', 'Please enter a valid phone number.', 'error');
+      Swal.fire("Error", "Please enter a valid phone number.", "error");
       return false;
     }
   }
@@ -22,37 +22,58 @@ function checkform() {
     if (postalCode.test(codigo)) {
       return true;
     } else {
-      Swal.fire('Error', 'Please enter a valid postal code.', 'error');
+      Swal.fire("Error", "Please enter a valid postal code.", "error");
       return false;
     }
   }
 
   // Obtén los valores de los campos
-  var fullName = document.getElementById('billing-name').value;
-  var email = document.getElementById('billing-email-address').value;
-  var phone = document.getElementById('billing-phone').value;
-  var address = document.getElementById('billing-address').value;
-  var country = document.getElementById('billing-country').value;
-  var city = document.getElementById('billing-city').value;
-  var postalCode = document.getElementById('zip-code').value;
-  var paymentMethod = document.querySelector('input[name="pay-method"]:checked').value;
+  var fullName = document.getElementById("billing-name").value;
+  var email = document.getElementById("billing-email-address").value;
+  var phone = document.getElementById("billing-phone").value;
+  var address = document.getElementById("billing-address").value;
+  var country = document.getElementById("billing-country").value;
+  var city = document.getElementById("billing-city").value;
+  var postalCode = document.getElementById("zip-code").value;
+  var paymentMethod = document.querySelector(
+    'input[name="pay-method"]:checked'
+  ).value;
 
   // Verifica si todos los campos están completos y el email es correcto
-  if (fullName && address && country && city && isCorrectE(email) && isCorrectT(phone) && isCorrectZ(postalCode)) {
-    // Realiza la solicitud AJAX al script PHP
-    $.ajax({
-      type: "POST",
-      url: "../../../services/formLogin.php", // Reemplaza con la ruta correcta de tu script PHP
-      data: { email: email },
-      success: function(response) {
-        Swal.fire('Success', response, 'success');
-      },
-      error: function() {
-        Swal.fire('Error', 'Error during AJAX request.', 'error');
+  if (
+    fullName &&
+    address &&
+    country &&
+    city &&
+    isCorrectE(email) &&
+    isCorrectT(phone) &&
+    isCorrectZ(postalCode)
+  ) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../../../services/formLogin.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          // Respuesta del servidor
+          Swal.fire("Success", "We have sent an email to " + email, "success");
+        } else {
+          // Manejo de errores
+          Swal.fire(
+            "Error",
+            "Error during AJAX request: " + xhr.statusText,
+            "error"
+          );
+        }
       }
-    });
+    };
+    xhr.onerror = function () {
+      // Manejo de errores de red
+      Swal.fire("Error", "Network error during AJAX request", "error");
+    };
+    // Enviar la dirección de correo electrónico al script PHP
+    xhr.send("email=" + email);
   } else {
-    Swal.fire('Error', 'Please fill in all required fields with valid information.', 'error');
+    Swal.fire("Error", "The email address entered is not valid.", "error");
   }
 }
-
